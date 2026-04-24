195117b9a1b4c075589c162490941748ee8beb3b
AdityaKute7 <adikute0001@gmail.com>
2026-04-24 23:36:58 +0530

    Global exception handlers if any relate to bad logins

---
+++ turfbooking-backend/src/main/java/com/turfbooking/app/exception/SlotAlreadyBookedException.java
@@ -0,0 +1,16 @@
+package com.turfbooking.app.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Thrown when a requested slot overlaps with an existing booking.
+ * Results in HTTP 409 Conflict response.
+ */
+@ResponseStatus(HttpStatus.CONFLICT)
+public class SlotAlreadyBookedException extends RuntimeException {
+
+    public SlotAlreadyBookedException(String message) {
+        super(message);
+    }
+}
