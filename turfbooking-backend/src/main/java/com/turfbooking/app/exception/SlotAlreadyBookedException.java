55306cec76aeebe9bdc9b36d79297ce04494f44a
AdityaKute7 <adikute0001@gmail.com>
2026-04-24 23:37:48 +0530

    SlotAlreadyBookedException Handeler

--- turfbooking-backend/src/main/java/com/turfbooking/app/exception/SlotAlreadyBookedException.java
+++ turfbooking-backend/src/main/java/com/turfbooking/app/exception/SlotAlreadyBookedException.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 /**
  * Thrown when a requested slot overlaps with an existing booking.
  * Results in HTTP 409 Conflict response.
+ * 
  */
 @ResponseStatus(HttpStatus.CONFLICT)
 public class SlotAlreadyBookedException extends RuntimeException {
